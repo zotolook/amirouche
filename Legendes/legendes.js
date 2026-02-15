44cd1819d97af94da4cf9936edea4c73c4fa5841
zotolook <lin.dag@yahoo.com>
2026-02-15 18:07:39 +0100

    commit...

---
+++ Legendes/legendes.js
@@ -0,0 +1,32 @@
+// 1- Gog et Magog
+const souratesGogMagog = [
+    18, 18, 18, 18, 18, 18, 18,
+    21, 21,];
+const versetsGogMagog = [
+    94, 95, 96, 97, 98, 99, 100,
+    95, 96,];
+
+// 2- Les gens de la caverne
+const souratesCaverne = [18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18,];
+const versetsCaverne = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,];
+
+// 3- Les deux hommes
+const souratesLesDeuxHommes = [18,18,18,18,18,18,18,18,18,18,18,18,18,];
+const versetsLesDeuxHommes = [32,33,34,35,36,37,38,39,40,41,42,43,44,];
+
+// 4- Le serviteur et Moïse
+const souratesServiteurMoise = [18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,];
+const versetsServiteurMoise = [60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,];
+
+// 5- Le Bi-cornu   قصة ذو القرنين 
+const souratesBicornu = [18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,];
+const versetsBicornu = [83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,];
+
+// 6- Luqman, un père exemplaire لقمان 
+const souratesLuqman = [31,31,31,31,31,31,31,31];
+const versetsLuqman = [12,13,14,15,16,17,18,19,];
+
+// 7-  Les gens de la cité يوحنا, بولص و شمعون 
+const souratesGensCite = [36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,];
+const versetsGensCite = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,];
+
