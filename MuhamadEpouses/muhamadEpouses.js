8bd6c5a9cb8552c477b47bf04f9a2e166e05d92c
zotolook <lin.dag@yahoo.com>
2026-04-14 13:03:54 +0200

    commit 28

---
+++ MuhamadEpouses/muhamadEpouses.js
@@ -0,0 +1,104 @@
+// 0- Femmes du prophète Muhamad
+const souratesMuhamadEpousesFemmes = [
+    "1- Le statut social des femmes du prophète",  33,33, 
+    "2- Le comportement des femmes du prophète", 33, 33,33,33,
+    "3- Les récompenses aux femmes du prophète", 33, 33,
+    "4- Les chatiments reservés aux femmes du prophète", 33
+];
+const versetsMuhamadEpousesFemmes = [
+    "...", 32, 6,
+    "...", 28,33,34,53, 
+    "...",29, 31,
+    "...",  30,
+];
+const ahadithsMuhamadEpousesFemmes = [];
+
+// 1- Khadidja
+const souratesMuhamadEpousesKhadidja = [
+  "Le Coran ne cite aucune épouse du Prophète par son nom, y compris Khadija.",
+];
+const versetsMuhamadEpousesKhadidja = [
+  "Certains versets parlent des épouses du Prophète en général, (voir femmes du prophète). Les informations sur elle viennent uniquement de hadiths (voir Ahadiths).",
+];
+const ahadithsMuhamadEpousesKhadidja = [
+  // Le mariage
+
+  {
+    sujet: " Le mariage ",
+    auteur: " al-Waqidi (Kitab al-Maghazi) ",
+    numero: "(m. 823)",
+    texteArabe:
+      " فَذَكَرَتْ ذَلِكَ لِأَبِيهَا، فَأَبَى.فَدَعَتْهُ إِلَى طَعَامٍ، فَسَقَتْهُ خَمْرًا حَتَّى سَكِرَ،فَلَمَّا سَكِرَ زَوَّجَتْهُ مِنْ رَسُولِ اللَّهِ ﷺ،فَلَمَّا أَفَاقَ قَالَ: مَا هَذَا  الُوا: زَوَّجْتَ مُحَمَّدَ بْنَ عَبْدِ اللَّهِ خَدِيجَةَ.فَأَنْكَرَ ذَلِكَ، فَقِيلَ لَهُ: قَدْ كَانَ ذَلِكَ. ",
+    texteFrancais: " Ils ont rapporté que Khadija fit venir son père, Khuwaylid ibn Asad.Elle lui fit boire du vin jusqu’à ce qu’il soit ivre.Puis elle se maria elle-même avec le Messager de Dieu ﷺ.Lorsqu’il reprit ses esprits, il nia cela.On lui dit alors : “Tu l’as bien mariée à lui.”",
+    lien: "https://archive.org/search?query=tarikh+al+tabari+arabic",
+    autres:
+      " Al-Tabari — Tārīkh al-Rusul wa al-Mulūk vol. 2. Pages p. 281–282 (selon éditions)",
+  },
+];
+
+// 2- Saouda
+const souratesMuhamadEpousesSaouda = [];
+const versetsMuhamadEpousesSaouda = [];
+const ahadithsMuhamadEpousesSaouda = [];
+
+// 3- Aicha
+const souratesMuhamadEpousesAicha = [];
+const versetsMuhamadEpousesAicha = [];
+const ahadithsMuhamadEpousesAicha = [];
+
+// 4- Hafsa
+const souratesMuhamadEpousesHafsa = [];
+const versetsMuhamadEpousesHafsa = [];
+const ahadithsMuhamadEpousesHafsa = [];
+
+// 5- Zaynab bint Khuzayma
+const souratesMuhamadEpousesZaynebKhuzayma = [];
+const versetsMuhamadEpousesZaynebKhuzayma = [];
+const ahadithsMuhamadEpousesZaynebKhuzayma = [];
+
+// 6- Umm Salama
+const souratesMuhamadEpousesUmSalama = [];
+const versetsMuhamadEpousesUmSalama = [];
+const ahadithsMuhamadEpousesUmSalama = [];
+
+// 7- Zaynab bent djahch
+const souratesMuhamadEpousesZaynebDhahsh = [];
+const versetsMuhamadEpousesZaynebDhahsh = [];
+const ahadithsMuhamadEpousesZaynebDhahsh = [];
+
+// 8-Juwayriya bint al-Harith
+const souratesMuhamadEpousesJuwayriya = [];
+const versetsMuhamadEpousesJuwayriya = [];
+const ahadithsMuhamadEpousesJuwayriya = [];
+
+// 9- Umm Habiba
+const souratesMuhamadEpousesUmHabiba = [];
+const versetsMuhamadEpousesUmHabiba = [];
+const ahadithsMuhamadEpousesUmHabiba = [];
+
+// 10- Safiyya bint Huyayy
+const souratesMuhamadEpousesSafiya = [];
+const versetsMuhamadEpousesSafiya = [];
+const ahadithsMuhamadEpousesSafiya = [];
+
+// 11-Maymuna bint al-Harith
+const souratesMuhamadEpousesMaymuna = [];
+const versetsMuhamadEpousesMaymuna = [];
+const ahadithsMuhamadEpousesMaymuna = [];
+
+// 12- Mariya la copte
+const souratesMuhamadEpousesMariya = [];
+const versetsMuhamadEpousesMariya = [];
+const ahadithsMuhamadEpousesMariya = [];
+
+// 13- Rayhana bint Zayd (ريحانة بنت زيد)
+const souratesMuhamadEpousesRayhana = [];
+const versetsMuhamadEpousesRayhana = [];
+const ahadithsMuhamadEpousesRayhana = [];
+
+
+// 14- mariage d’offrande
+const souratesMuhamadEpousesMariageOffrande = [];
+const versetsMuhamadEpousesMariageOffrande = [];
+const ahadithsMuhamadEpousesMariageOffrande = [];
+
